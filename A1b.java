42f8f78bb176ded45318b9c0c1f8612e93e3b6dd
akashnow <157609701+akashnow@users.noreply.github.com>
2024-09-10 14:36:25 +0530

    Add files via upload

---
+++ A1b.java
@@ -0,0 +1,10 @@
+//Write a Java program to print a message “Welcome (Your name here) to Java Programming”, where your name is stored in a variable.
+
+class A1b
+{
+public static void main(String args [])
+{
+String s="Akash";
+System.out.println(“Welcome  to Java Programming”);
+}
+} 
